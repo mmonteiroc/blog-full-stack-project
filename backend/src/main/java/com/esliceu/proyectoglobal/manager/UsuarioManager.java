6efde12f869a562ff890bc50fd3f2c848189f692
mmonteirocl <miguelmonteiroclaveri@gmail.com>
2020-02-18 20:18:27 +0100

    Manager de la entidad Usuario creada

---
+++ backend/src/main/java/com/esliceu/proyectoglobal/manager/UsuarioManager.java
@@ -0,0 +1,47 @@
+package com.esliceu.proyectoglobal.manager;
+
+import com.esliceu.proyectoglobal.entity.Usuario;
+import com.esliceu.proyectoglobal.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Code created by: mmonteirocl
+ * Email: miguelmonteiroclaveri@gmail.com
+ * Date: 18/02/2020
+ * Package: com.esliceu.proyectoglobal.manager
+ * Project: BackEnd
+ */
+@Service
+public class UsuarioManager {
+
+    @Autowired
+    UsuarioRepository usuarioRepository;
+
+    public List<Usuario> findAll() {
+        List<Usuario> toReturn = new LinkedList<>();
+
+        for (Usuario user : usuarioRepository.findAll()) {
+            toReturn.add(user);
+        }
+
+        return toReturn;
+    }
+
+    public Optional<Usuario> findById(Long id) {
+        return usuarioRepository.findById(id);
+    }
+
+    public void saveOrUpdate(Usuario user) {
+        usuarioRepository.save(user);
+    }
+
+    public void delete(Usuario user) {
+        usuarioRepository.delete(user);
+    }
+
+}
