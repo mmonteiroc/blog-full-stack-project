7fe3eda6f959a56fead441d2ce2054fb5bb9c87d
mmonteirocl <miguelmonteiroclaveri@gmail.com>
2020-02-18 17:45:55 +0100

    Creamos los DAO mediante la interfaz CrudRepository.  De momento continuamos con los metodos implementados por defecto
    PAIR PROGRAMING - MIGUEL MONTEIRO & MIGUEL A. ARRONES

---
+++ backend/src/main/java/com/esliceu/proyectoglobal/repository/PostRepository.java
@@ -0,0 +1,15 @@
+package com.esliceu.proyectoglobal.repository;
+
+import com.esliceu.proyectoglobal.entity.Post;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Code created by: mmonteirocl
+ * Email: miguelmonteiroclaveri@gmail.com
+ * Date: 18/02/2020
+ * Package: com.esliceu.proyectoglobal.repository
+ * Project: BackEnd
+ */
+public interface PostRepository extends CrudRepository<Post, Long> {
+
+}
---
+++ backend/src/main/java/com/esliceu/proyectoglobal/repository/UsuarioRepository.java
@@ -0,0 +1,14 @@
+package com.esliceu.proyectoglobal.repository;
+
+import com.esliceu.proyectoglobal.entity.Usuario;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Code created by: mmonteirocl
+ * Email: miguelmonteiroclaveri@gmail.com
+ * Date: 18/02/2020
+ * Package: com.esliceu.proyectoglobal.repository
+ * Project: BackEnd
+ */
+public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
+}
