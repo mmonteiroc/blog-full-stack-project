994216f1b30cba9b956024fb2dc2c3394f05b70c
mmonteirocl <miguelmonteiroclaveri@gmail.com>
2020-02-18 11:58:52 +0100

    Creamos un LAYOUT para la parte publica de nuestra aplicacion PAIR PROGRAMING - MIGUEL MONTEIRO & MIGUEL A. ARRONES

---
+++ frontend/src/layouts/PublicLayout.vue
@@ -0,0 +1,29 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+
+    <q-header elevated class="bg-primary text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar square>
+            <img src="https://i.ya-webdesign.com/images/carrots-png-emoji-3.png">
+          </q-avatar>
+          Es liceu - Restaurant
+        </q-toolbar-title>
+        <q-btn to="/login" label="Login" class="bg-dark"/>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view/>
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {}
+    }
+  }
+</script>
