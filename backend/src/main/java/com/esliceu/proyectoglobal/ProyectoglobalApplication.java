fe7b15865e9cb57bdb32f3c1ad20de1e83e30144
maarrones <miguelaarrones@icloud.com>
2020-02-18 17:00:33 +0100

    Commit de prova.

--- backend/src/main/java/com/esliceu/proyectoglobal/ProyectoglobalApplication.java
+++ backend/src/main/java/com/esliceu/proyectoglobal/ProyectoglobalApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProyectoglobalApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(ProyectoglobalApplication.class, args);
     }
