29002729408967b88f57fecbadd69657a86b8060
mmonteirocl <miguelmonteiroclaveri@gmail.com>
2020-02-18 20:23:20 +0100

    Creamos el controller de Usuario

---
+++ backend/src/main/java/com/esliceu/proyectoglobal/controller/UsuarioController.java
@@ -0,0 +1,16 @@
+package com.esliceu.proyectoglobal.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Code created by: mmonteirocl
+ * Email: miguelmonteiroclaveri@gmail.com
+ * Date: 18/02/2020
+ * Package: com.esliceu.proyectoglobal.controller
+ * Project: BackEnd
+ */
+@RestController
+public class UsuarioController {
+
+
+}
