85910b632b470c5e1b04196861c58010902e6ace
mmonteirocl <miguelmonteiroclaveri@gmail.com>
2020-02-19 11:13:40 +0100

    Creacion del RestController

---
+++ backend/src/main/java/com/esliceu/proyectoglobal/controller/PostController.java
@@ -0,0 +1,16 @@
+package com.esliceu.proyectoglobal.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Code created by: mmonteirocl
+ * Email: miguelmonteiroclaveri@gmail.com
+ * Date: 19/02/2020
+ * Package: com.esliceu.proyectoglobal.controller
+ * Project: BackEnd
+ */
+@RestController
+public class PostController {
+
+
+}
