27ad59009fcf1bf280d9fd830e01d128a934f0c4
mmonteirocl <miguelmonteiroclaveri@gmail.com>
2020-02-21 00:43:11 +0100

    Creamos el filtro de nuestro token el cual tambien validamos antes de TODAS las peticiones a los endpoints privados PAIR PROGRAMING MIGUEL MONTEIRO & MIGUEL A ARRONES

--- backend/src/main/java/com/esliceu/proyectoglobal/filter/TokenFilter.java
+++ backend/src/main/java/com/esliceu/proyectoglobal/filter/TokenFilter.java
@@ -1,5 +1,7 @@
 package com.esliceu.proyectoglobal.filter;
 
+import com.esliceu.proyectoglobal.manager.TokenManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
@@ -13,11 +15,30 @@ import javax.servlet.http.HttpServletResponse;
  * GitHub: https://github.com/miguelaarrones
  */
 public class TokenFilter implements HandlerInterceptor {
+    @Autowired
+    private TokenManager tokenManager;
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        /*
-         * TODO Implementar el filtro para los token.
-         * */
+
+        if (request.getMethod().equals("OPTIONS"))return true;
+
+
+        String authorization = request.getHeader("Authorization");
+
+        if (authorization != null && !authorization.isEmpty()) {
+            String token = authorization.replace("Bearer ", "");
+
+            boolean valid =  tokenManager.validateToken(token);
+            if (!valid){
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                return false;
+            }else {
+                response.setStatus(HttpServletResponse.SC_OK);
+                return true;
+            }
+        }
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         return false;
     }
 }
